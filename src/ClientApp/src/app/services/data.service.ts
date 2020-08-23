f70b555dace745fd335a5ec8c4735d490c9eee77
Ion Dormenco <idormenco@gmail.com>
2020-08-23 11:54:42 +0300

    Polling stations on a purple background should be better to have separate cards
    closes #45

--- src/ClientApp/src/app/services/data.service.ts
+++ src/ClientApp/src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 import { ErrorService } from './error.service';
@@ -22,6 +22,13 @@ export interface StaticData {
   votersGuide: VotingGuide;
 }
 
+export interface AssignedAddress {
+  houseNumbers: string;
+  remarks: string;
+  street: string;
+  streetCode: string;
+}
+
 export interface PollingStation {
   id: number;
   latitude: number;
@@ -31,7 +38,7 @@ export interface PollingStation {
   pollingStationNumber: string;
   institution: string;
   address: string;
-  assignedAddresses: string[];
+  assignedAddresses: AssignedAddress[];
 }
 
 export interface PollingStationGroup {
