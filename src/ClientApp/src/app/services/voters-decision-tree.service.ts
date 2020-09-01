4824ba7eb0dec9cb980a0a07abc9c9869ad5a338
Alex Stefanescu <s.alexandra.stefanescu@gmail.com>
2020-09-01 17:38:05 +0300

    Add initial decision tree impl

--- src/ClientApp/src/app/services/voters-decision-tree.service.ts
+++ src/ClientApp/src/app/services/voters-decision-tree.service.ts
@@ -47,42 +47,150 @@ export class VotersDecisionTreeService {
 
 
   getDecisionTree$(): Observable<OperatorDecisionTree> {
-    // TODO: put it here
     const data: OperatorDecisionTree = {
-
-      '0': {
-        id: '0',
-        label: 'Esti cetatean roman?',
-        options: [
-          '00',
-        ]
-      },
-      '00': {
-        id: '00',
-        text: 'nu poti vota!'
-      },
-      '01': {
-        id: '01',
-        votersOptionId: 5
-      },
-
-      '1': {
-        id: '1',
-        label: 'Esti cetatean UE?',
-        options: [
-          '01'
-        ]
-      },
-
-
-
       'initial': {
         id: 'initial',
         options: [
           '0',
           '1'
         ]
-      }
+      },
+      '0': {
+        id: '0',
+        label: 'Ești cetățean român',
+        options: [
+          '00',
+          '01'
+        ]
+      },
+      '1': {
+        id: '1',
+        label: 'Ești cetățean al unei alte țări din Uniunea Europeană',
+        options: [
+          '10',
+          '11'
+        ]
+      },
+      '00': {
+        id: '00',
+        label: 'Nu te vei afla în România pe 27 septembrie',
+        options: [
+          '000'
+        ]
+      },
+      '01': {
+        id: '01',
+        label: 'Te vei afla în Romania pe 27 septembrie',
+        options: [
+          '010',
+          '011'
+        ]
+      },
+      '000': {
+        id: '000',
+        votersOptionId: 1
+      },
+      '010': {
+        id: '010',
+        label: 'Locuiești la adresa din buletin',
+        options: [
+          '0100'
+        ]
+      },
+      '0100': {
+        id: '0100',
+        votersOptionId: 2
+      },
+      '011': {
+        id: '011',
+        label: 'Locuiești la altă adresă decât cea din buletin',
+        options: [
+          '0110',
+          '0111'
+        ]
+      },
+      '0110': {
+        id: '0110',
+        label: 'Ai pe spatele buletinului un autocolant cu viza de flotant',
+        options: [
+          '01100'
+        ]
+      },
+      '01100': {
+        id: '01100',
+        votersOptionId: 3
+      },
+      '0111': {
+        id: '0111',
+        label: 'Nu ai viză de flotant pe spatele buletinului',
+        options: [
+          '01110',
+          '01111'
+        ]
+      },
+      '01110': {
+        id: '01110',
+        label: 'Te poate lua cineva în spațiu',
+        options: [
+          '011100'
+        ]
+      },
+      '011100': {
+        id: '011100',
+        votersOptionId: 4
+      },
+      '01111': {
+        id: '01111',
+        label: 'Nu ai pe cineva care te poate lua în spațiu',
+        options: [
+          '011110'
+        ]
+      },
+      '011110': {
+        id: '011110',
+        votersOptionId: 5
+      },
+      '10': {
+        id: '10',
+        label: 'Ești în evidența Inspectoratului General pentru Imigrări',
+        options: [
+          '100',
+          '101'
+        ]
+      },
+      '100': {
+        id: '100',
+        label: 'Adresa la care locuiești nu se va schimba în intervalul 3-27 septembrie',
+        options: [
+          '1010'
+        ]
+      },
+      '1000': {
+        id: '1000',
+        votersOptionId: 6
+      },
+      '101': {
+        id: '101',
+        label: 'Adresa la care locuiești nu se va schimba în intervalul 3-27 septembrie',
+        options: [
+          '1010'
+        ]
+      },
+      '1010': {
+        id: '1000',
+        votersOptionId: 7
+      },
+      '11': {
+        id: '11',
+        label: 'Nu ești în evidența Inspectoratului General pentru Imigrări',
+        options: [
+          '110',
+        ]
+      },
+      '110': {
+        id: '110',
+        votersOptionId: 8
+      },
     };
 
     return of(data);
@@ -107,7 +215,7 @@ export class VotersDecisionTreeService {
     filter(([tree]) => treeIsErrorFree(tree)),
     map(([tree, { previousBranchIds }]) =>
       isInitialDecision(previousBranchIds)
-        ? 'Incepe prin a alege una din optiunile de mai jos'
+        ? 'Începe prin a alege una din opțiunile de mai jos'
         : `${previousBranchIds
           .map(entityId => {
             return tree[entityId].label;
