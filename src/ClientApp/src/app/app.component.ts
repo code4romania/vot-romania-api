28b48ffdb06f4b176e5f7e417145d5e8cc7b6081
Ion Dormenco <idormenco@gmail.com>
2020-09-02 10:59:07 +0300

    Implement i18n

--- src/ClientApp/src/app/app.component.ts
+++ src/ClientApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LoadDataAction } from './state/actions';
+import { TranslateService } from '@ngx-translate/core';
+import defaultLanguage from '../assets/i18n/ro.json';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,11 @@ import { LoadDataAction } from './state/actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<any>) {
-
+  constructor(private store: Store<any>, translate: TranslateService) {
+    translate.setTranslation('ro', defaultLanguage);
+    translate.setDefaultLang('ro');
   }
-  title = 'app';
+
   ngOnInit(): void {
     this.store.dispatch(new LoadDataAction());
   }
--- src/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -4,6 +4,7 @@ import { ApplicationState } from '../state/reducers';
 import { Store, select } from '@ngrx/store';
 import { getLanguages, getSelectedLanguage } from '../state/selectors';
 import { ChangeSelectedLanguage } from '../state/actions';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -14,12 +15,13 @@ export class NavMenuComponent {
   selectedLanguage$: Observable<string>;
   availableLanguages$: Observable<string[]>;
 
-  constructor(private store: Store<ApplicationState>) {
+  constructor(private store: Store<ApplicationState>, private translate: TranslateService) {
     this.availableLanguages$ = this.store.pipe(select(getLanguages));
     this.selectedLanguage$ = this.store.pipe(select(getSelectedLanguage));
   }
 
   changeSelectedLanguageTo(language: string): void {
     this.store.dispatch(new ChangeSelectedLanguage(language));
+    this.translate.use(language.toLocaleLowerCase());
   }
 }
